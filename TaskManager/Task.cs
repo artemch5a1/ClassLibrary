339906c3ed6db839a7dd5639280dea0d3a7a053d
artemch5a1 <artemch5a1@yandex.ru>
2025-03-07 00:20:13 +0300

    models build

---
+++ TaskManager/Task.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager
+{
+    public class Team
+    {
+        private static int _nextId = 1;
+        public Team(string name)
+        {
+            this.id = _nextId++;
+            this.name = name;
+        }
+
+        public int id { get; protected set; }
+        public string name { get; set; }
+        public virtual List<Member> members { get; protected set; } = new List<Member>();
+        public virtual List<Project> projects { get; protected set; } = new List<Project>();
+
+    }
+    public class Project
+    {
+        private static int _nextId = 1;
+        public Project(string name, string description)
+        {
+            this.name = name;
+            this.description = description;
+            this.id = _nextId++;
+        }
+
+        public int id { get; protected set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public virtual Team team { get; protected set; }
+        public virtual List<Task> tasks { get; protected set; } = new List<Task>();
+    }
+    public class Member
+    {
+        private static int _nextId = 1;
+        public Member(string name)
+        {
+            this.id = _nextId++;
+            this.name = name;
+        }
+
+        public int id { get; set; }
+        public string name { get; set; }
+        public virtual Team team { get; protected set; }
+        public List<Task> tasks { get; protected set; } = new List<Task>();
+    }
+    public class Task
+    {
+        private static int _nextId = 1;
+        public Task(string name, string description, Status status, Priority priority)
+        {
+            this.id = _nextId++;
+            this.name = name;
+            this.description = description;
+            this.status = status;
+            this.priority = priority;
+        }
+
+        public int id {  get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        Status status { get; set; }
+        Priority priority { get; set; }
+        public DateTime dateset { get; protected set; } = DateTime.Now;
+        public virtual Project project { get; protected set; }
+        public virtual List<Member> performers { get; protected set;} = new List<Member>();
+    }
+    public enum Status
+    {
+        New,
+        InProgress,
+        Completed,
+        Cancelled 
+    }
+    public enum Priority
+    {
+        Low,
+        Medium,
+        High
+    }
+
+    
+}
