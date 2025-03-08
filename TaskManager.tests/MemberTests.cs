df706b5390194b236944d930914ea5f21ece3090
artemch5a1 <artemch5a1@yandex.ru>
2025-03-08 05:51:04 +0300

    Done

--- TaskManager.tests/MemberTests.cs
+++ TaskManager.tests/MemberTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,74 @@ using System.Threading.Tasks;
 
 namespace TaskManager.tests
 {
+    
+    [TestClass]
     public class MemberTests
     {
+        // Тестирование метода AddTask
+        [TestClass]
+        public class AddTaskTests
+        {
+            // Проверка добавления задачи участнику
+            // Ожидание: задача добавляется в список задач участника, а участник добавляется в список исполнителей задачи.
+            [TestMethod]
+            public void AddTask_ValidTask_AddMemberInTaskAddTaskInMember()
+            {
+                //arrange
+                Team team = new Team("test");
+                Task task = new Task("test", "test", Priority.High);
+                team.CreateTask(task);
+                Member member = new Member("test");
+                team.AddMember(member);
+
+                //act
+                member.AddTask(task);
+
+                //assert
+                Assert.AreEqual(team.GetTask(task.id).performers, task.performers); // Проверка, что участник добавлен в задачу
+                Assert.IsTrue(task.performers.Contains(member)); // Проверка, что участник есть в списке исполнителей задачи
+                Assert.IsTrue(member.tasks.Contains(task)); // Проверка, что задача есть в списке задач участника
+            }
+
+            // Проверка добавления задачи, которая принадлежит другой команде
+            // Ожидание: выбрасывается исключение ArgumentException с сообщением "This task does not belong to member's team".
+            [TestMethod]
+            public void AddTask_AnotherTeamTask_ThrowsArgumentException()
+            {
+                //arrange
+                Team team = new Team("test");
+                Task task = new Task("test", "test", Priority.High);
+                team.CreateTask(task);
+                Team team1 = new Team("test");
+                Member member = new Member("test");
+                team1.AddMember(member);
+                var expected = new ArgumentException("This task does not belong to member's team");
+
+                //act & assert
+                var actual = Assert.ThrowsException<ArgumentException>(() => member.AddTask(task));
+
+                //assert
+                Assert.AreEqual(expected.Message, actual.Message); // Проверка, что сообщение исключения соответствует ожидаемому
+            }
+
+            // Проверка добавления null-задачи
+            // Ожидание: выбрасывается исключение ArgumentNullException с сообщением "task is null".
+            [TestMethod]
+            public void AddTask_NullTask_ThrowsArgumentNullException()
+            {
+                //arrange
+                Team team = new Team("test");
+                Task task = null;
+                Member member = new Member("test");
+                team.AddMember(member);
+                var expected = new ArgumentNullException("task is null");
+
+                //act & assert
+                var actual = Assert.ThrowsException<ArgumentNullException>(() => member.AddTask(task));
+
+                //assert
+                Assert.AreEqual(expected.Message, actual.Message); // Проверка, что сообщение исключения соответствует ожидаемому
+            }
+        }
     }
 }
--- TaskManager.tests/TaskTests.cs
+++ TaskManager.tests/TaskTests.cs
@@ -5,187 +5,152 @@ using System.Linq;
 
 namespace TaskManager.tests
 {
-
-
-
+    // Тестирование функционала класса Task, связанного с изменением статуса и строковым представлением задачи.
     [TestClass]
     public class TaskTests
     {
-        [TestMethod]
-        public void ChangeStatus_NullTask_ThrowsArgumentNullExceptionNotContent()
+        // Тестирование метода ChangeTask
+        [TestClass]
+        public class ChangeStatusTests
         {
-            //arrange
-            Task task = null;
-            Task task1 = new Task("s", "s", Priority.High);
-            var expected = new ArgumentNullException("Not content");
-            //act & assert
-            var actual = Assert.ThrowsException<ArgumentNullException>(() => task1.ChangeTask(task));
-            //act
-            Assert.AreEqual(expected.Message, actual.Message);
+            // Проверка изменения задачи с передачей null в качестве аргумента
+            // Ожидание: выбрасывается исключение ArgumentNullException с сообщением "Not content".
+            [TestMethod]
+            public void ChangeStatus_NullTask_ThrowsArgumentNullExceptionNotContent()
+            {
+                //arrange
+                Task task = null;
+                Task task1 = new Task("s", "s", Priority.High);
+                var expected = new ArgumentNullException("Not content");
+
+                //act & assert
+                var actual = Assert.ThrowsException<ArgumentNullException>(() => task1.ChangeTask(task));
+
+                //assert
+                Assert.AreEqual(expected.Message, actual.Message); // Проверка, что сообщение исключения соответствует ожидаемому
+            }
+
+            // Проверка изменения задачи с передачей валидного объекта Task
+            // Ожидание: значения полей task1 изменяются на значения полей task2, но объекты остаются разными.
+            [TestMethod]
+            public void ChangeStatus_ValidTask_Task1ValuesEqualTask2EqualButTask1NotEqualTask2()
+            {
+                //arrange
+                Task task1 = new Task("g", "g", Priority.Low);
+                task1.status = Status.Cancelled;
+                Task task2 = new Task("s", "s", Priority.High);
+                bool expected = true;
+
+                //act
+                task1.ChangeTask(task2);
+                bool actual = (task1.name == task2.name && task1.description == task2.description && task1.priority == task2.priority && task1.status == task2.status) && (!task1.Equals(task2));
+
+                //assert
+                Assert.AreEqual(expected, actual); // Проверка, что значения полей равны, но объекты разные
+            }
         }
-        [TestMethod]
-        public void ChangeStatus_ValidTask_Task1ValuesEqualTask2EqualButTask1NotEqualTask2()
+
+        // Тестирование метода ToString
+        [TestClass]
+        public class ToStringTests
         {
-            //arrange
-            Task task1 = new Task("g", "g", Priority.Low);
-            task1.status = Status.Cancelled;
-            Task task2 = new Task("s", "s", Priority.High);
-            bool expected = true;
-            //act
-            task1.ChangeTask(task2);
-            bool actual = (task1.name == task2.name && task1.description == task2.description && task1.priority == task2.priority && task1.status == task2.status) && (!task1.Equals(task2));
-            //assert
-            Assert.AreEqual(expected, actual);
+            // Проверка строкового представления задачи с null-проектом и не-null командой
+            // Ожидание: возвращается строка с корректным представлением задачи, включая название команды.
+            [TestMethod]
+            public void ToString_TaskWithNullProjAndNotNullTeam_CorrectString()
+            {
+                //arrange
+                Task task1 = new Task("test", "test ToString", Priority.Low);
+                Team team = new Team("testTeam");
+                team.CreateTask(task1);
+                string expected = $"Задача id = {task1.id}: test\n" +
+                    $"Описание: test ToString\n" +
+                    $"Статус: New\n" +
+                    $"Приоретет: Low\n" +
+                    $"Дата создания: {task1.dateset.ToString()}\n" +
+                    $"Название проекта: нет\n" +
+                    $"Название команды: testTeam";
+
+                //act
+                string actual = task1.ToString();
+
+                //assert
+                Assert.AreEqual(expected, actual); // Проверка, что строковое представление задачи корректно
+            }
+
+            // Проверка строкового представления задачи с null-проектом и null-командой
+            // Ожидание: возвращается строка с корректным представлением задачи, где проект и команда указаны как "нет".
+            [TestMethod]
+            public void ToString_TaskWithNullProjAndNullTeam_CorrectString()
+            {
+                //arrange
+                Task task1 = new Task("test", "test ToString", Priority.Low);
+
+                string expected = $"Задача id = {task1.id}: test\n" +
+                    $"Описание: test ToString\n" +
+                    $"Статус: New\n" +
+                    $"Приоретет: Low\n" +
+                    $"Дата создания: {task1.dateset.ToString()}\n" +
+                    $"Название проекта: нет\n" +
+                    $"Название команды: нет";
+
+                //act
+                string actual = task1.ToString();
+
+                //assert
+                Assert.AreEqual(expected, actual); // Проверка, что строковое представление задачи корректно
+            }
+
+            // Проверка строкового представления задачи с не-null проектом и null-командой
+            // Ожидание: возвращается строка с корректным представлением задачи, включая название проекта.
+            [TestMethod]
+            public void ToString_TaskWithNotNullProjAndNullTeam_CorrectString()
+            {
+                //arrange
+                Task task1 = new Task("test", "test ToString", Priority.Low);
+                Project project = new Project("test", "test p");
+                task1.SetProject(project);
+                string expected = $"Задача id = {task1.id}: test\n" +
+                    $"Описание: test ToString\n" +
+                    $"Статус: New\n" +
+                    $"Приоретет: Low\n" +
+                    $"Дата создания: {task1.dateset.ToString()}\n" +
+                    $"Название проекта: test\n" +
+                    $"Название команды: нет";
+
+                //act
+                string actual = task1.ToString();
+
+                //assert
+                Assert.AreEqual(expected, actual); // Проверка, что строковое представление задачи корректно
+            }
+
+            // Проверка строкового представления задачи с не-null проектом и не-null командой
+            // Ожидание: возвращается строка с корректным представлением задачи, включая название проекта и команды.
+            [TestMethod]
+            public void ToString_TaskWithNotNullProjAndNotNullTeam_CorrectString()
+            {
+                //arrange
+                Task task1 = new Task("test", "test ToString", Priority.Low);
+                Project project = new Project("test", "test p");
+                task1.SetProject(project);
+                Team team = new Team("testTeam");
+                team.CreateTask(task1);
+                string expected = $"Задача id = {task1.id}: test\n" +
+                    $"Описание: test ToString\n" +
+                    $"Статус: New\n" +
+                    $"Приоретет: Low\n" +
+                    $"Дата создания: {task1.dateset.ToString()}\n" +
+                    $"Название проекта: test\n" +
+                    $"Название команды: testTeam";
+
+                //act
+                string actual = task1.ToString();
+
+                //assert
+                Assert.AreEqual(expected, actual); // Проверка, что строковое представление задачи корректно
+            }
         }
-        [TestMethod]
-        public void ToString_TaskWithNullProjAndNotNullTeam_CorrectString()
-        {
-            //arrange
-            Task task1 = new Task("test", "test ToString", Priority.Low);
-            Team team = new Team("testTeam");
-            team.CreateTask(task1);
-            string expected = $"Задача id = {task1.id}: test\n" +
-                $"Описание: test ToString\n" +
-                $"Статус: New\n" +
-                $"Приоретет: Low\n" +
-                $"Дата создания: {task1.dateset.ToString()}\n" +
-                $"Название проекта: нет\n" +
-                $"Название команды: testTeam";
-
-            //act
-            string actual = task1.ToString();
-
-            //assert
-            Assert.AreEqual(expected, actual);
-        }
-        [TestMethod]
-        public void ToString_TaskWithNullProjAndNullTeam_CorrectString()
-        {
-            //arrange
-            Task task1 = new Task("test", "test ToString", Priority.Low);
-            
-            string expected = $"Задача id = {task1.id}: test\n" +
-                $"Описание: test ToString\n" +
-                $"Статус: New\n" +
-                $"Приоретет: Low\n" +
-                $"Дата создания: {task1.dateset.ToString()}\n" +
-                $"Название проекта: нет\n" +
-                $"Название команды: нет";
-
-            //act
-            string actual = task1.ToString();
-
-            //assert
-            Assert.AreEqual(expected, actual);
-        }
-        [TestMethod]
-        public void ToString_TaskWithNotNullProjAndNullTeam_CorrectString()
-        {
-            //arrange
-            Task task1 = new Task("test", "test ToString", Priority.Low);
-            Project project = new Project("test", "test p");
-            task1.SetProject(project);
-            string expected = $"Задача id = {task1.id}: test\n" +
-                $"Описание: test ToString\n" +
-                $"Статус: New\n" +
-                $"Приоретет: Low\n" +
-                $"Дата создания: {task1.dateset.ToString()}\n" +
-                $"Название проекта: test\n" +
-                $"Название команды: нет";
-
-            //act
-            string actual = task1.ToString();
-
-            //assert
-            Assert.AreEqual(expected, actual);
-        }
-         [TestMethod]
-        public void ToString_TaskWithNotNullProjAndNotNullTeam_CorrectString()
-        {
-            //arrange
-            Task task1 = new Task("test", "test ToString", Priority.Low);
-            Project project = new Project("test", "test p");
-            task1.SetProject(project);
-            Team team = new Team("testTeam");
-            team.CreateTask(task1);
-            string expected = $"Задача id = {task1.id}: test\n" +
-                $"Описание: test ToString\n" +
-                $"Статус: New\n" +
-                $"Приоретет: Low\n" +
-                $"Дата создания: {task1.dateset.ToString()}\n" +
-                $"Название проекта: test\n" +
-                $"Название команды: testTeam";
-
-            //act
-            string actual = task1.ToString();
-
-            //assert
-            Assert.AreEqual(expected, actual);
-        }
-        
-        
     }
-
-    
-
-    [TestClass]
-    public class MemberTest
-    {
-        [TestMethod]
-        public void AddTask_ValidTask_AddMemberInTaskAddTaskInMember()
-        {
-            //arrange
-            Team team = new Team("test");
-            Task task = new Task("test", "test", Priority.High);
-            team.CreateTask(task);
-            Member member = new Member("test");
-            team.AddMember(member);
-
-            //act
-            member.AddTask(task);
-
-
-            //assert
-            Assert.AreEqual(team.GetTask(task.id).performers, task.performers);
-            Assert.IsTrue(task.performers.Contains(member));
-            Assert.IsTrue(member.tasks.Contains(task));
-        }
-        [TestMethod]
-        public void AddTask_AnotherTeamTask_ThrowsArgumentException()
-        {
-            //arrange
-            Team team = new Team("test");
-            Task task = new Task("test", "test", Priority.High);
-            team.CreateTask(task);
-            Team team1 = new Team("test");
-            Member member = new Member("test");
-            team1.AddMember(member);
-            var expected = new ArgumentException("This task does not belong to member's team");
-
-            //act & assert
-            var actual = Assert.ThrowsException<ArgumentException>(() => member.AddTask(task));
-
-
-            //assert
-            Assert.AreEqual(expected.Message, actual.Message);
-        }
-        [TestMethod]
-        public void AddTask_NullTask_ThrowsArgumentNullException()
-        {
-            //arrange
-            Team team = new Team("test");
-            Task task = null;
-            Member member = new Member("test");
-            team.AddMember(member);
-            var expected = new ArgumentNullException("task is null");
-            //act & assert
-            var actual = Assert.ThrowsException<ArgumentNullException>(() => member.AddTask(task));
-
-            //assert
-            Assert.AreEqual(expected.Message, actual.Message);
-        }
-
-        
-    }
-
 }
+    
